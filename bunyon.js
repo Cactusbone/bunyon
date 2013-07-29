53d60b43742009ede96ed6b0ce6bbd2c683779a8
Philipp Dunkel <p.dunkel@durchblicker.at>
2013-07-29 12:51:44 +0200

    Network Logging

--- bunyon.js
+++ bunyon.js
@@ -5,3 +5,5 @@
 module.exports = require('./lib/log.js');
 module.exports.Serializers = require('./lib/serializers.js');
 module.exports.Stream = require('./lib/stream.js');
+module.exports.TCP = require('./lib/tcp.js');
+module.exports.UDP = require('./lib/udp.js');
